f3ddd7d0651a572014a2931820de6d522a3cd7d7
reecefenwick <reece@tagr.io>
2023-05-12 22:58:03 +1000

    update db script

--- src/db.ts
+++ src/db.ts
@@ -3,7 +3,8 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 export async function main() {
-  console.log(await prisma.user.count())
+  console.log(await prisma.answer.count())
+  console.log(await prisma.question.count())
 }
 
 main();
