75d0232aa4f9798d589fb321ac2034278aabe0fd
reecefenwick <reece@tagr.io>
2023-05-13 16:59:30 +1000

    things

--- src/service/questionService.ts
+++ src/service/questionService.ts
@@ -1,19 +1,19 @@
 import {prisma} from "../helpers/prismaClient";
-import {getSlackUserByEmail, sendQA} from "../slack-bot";
 import {Question} from "@prisma/client";
 
-type QuestionId = number;
-type UserId = number;
-/**
- * Keep track of the last question asked to a user
- */
-const askedQuestions: Record<UserId, QuestionId> = {}
 
-export const getNextUnansweredQuestion = async (userId: UserId): Promise<Question  | undefined> => {
+export const getNextUnansweredQuestion = async (userId: number): Promise<Question | undefined> => {
 
   const questions = await prisma.question.findMany();
 
-  const answeredQuestions = await prisma.answer.findMany({where: {userId: userId}});
+  const answeredQuestions = await prisma.answer.findMany({
+    orderBy: {
+      id: 'asc'
+    },
+    where: {
+      userId: userId
+    }
+  });
 
   const answeredQuestionIds = answeredQuestions.map(answer => answer.questionId);
 
