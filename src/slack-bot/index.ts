b834c7838bbf6ce0bbbeff1ab17aed3039322439
Connor Monaghan <connor_monaghan@hotmail.com>
2023-05-13 16:17:51 +1000

    ack at the start

--- src/slack-bot/index.ts
+++ src/slack-bot/index.ts
@@ -76,6 +76,8 @@ app.event('member_joined_channel', async ({ event, client }) => {
 app.action('submit_question', async ({ ack, say, body, client }) => {
   // eslint-disable-next-line
   // @ts-ignore
+
+  await ack();
   const questionIds = Object.keys(body.state.values.question_block);
   const userInfo = await client.users.info({ user: body.user.id });
   const email = userInfo.user.profile.email;
@@ -145,8 +147,6 @@ app.action('submit_question', async ({ ack, say, body, client }) => {
     }
 
   }
-
-  await ack();
 });
 
 export async function getSlackUserByEmail(email: string) {
