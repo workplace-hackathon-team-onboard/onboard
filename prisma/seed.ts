b5c6e7658dc847d4977754144a318f56dd44ed14
Brian Riwu Kaho <b.riwukaho@gmail.com>
2023-05-13 12:01:13 +1000

    update seeding script to seed answers

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,19 +1,18 @@
-import {prisma} from '../src/helpers/prismaClient'
+import { prisma } from '../src/helpers/prismaClient';
 
 // create an array of icebreaker questions
 const questions = [
-  {question: "What is your favorite food?"},
-  {question: "What is your favorite animal?"},
-  {question: "What is your favorite movie?"},
-  {question: "What is your favorite song?"},
-  {question: "What is your favorite TV show?"},
-  {question: "What is your favorite sport?"},
-]
-
+  { question: 'What is your favorite food?' },
+  { question: 'What is your favorite animal?' },
+  { question: 'What is your favorite movie?' },
+  { question: 'What is your favorite song?' },
+  { question: 'What is your favorite TV show?' },
+  { question: 'What is your favorite sport?' },
+];
 
 // create a function to insert questions into the database
 const insertQuestions = async () => {
-  for (let {question} of questions) {
+  for (let { question } of questions) {
     await prisma.question.upsert({
       create: {
         question,
@@ -22,55 +21,97 @@ const insertQuestions = async () => {
       update: {},
       where: {
         question,
-      }
+      },
     });
   }
-}
+};
 
 const insertUsers = async () => {
   const users = [
     {
-      name: "Connor",
-      email: "john@gmail.com",
+      name: 'Connor',
+      email: 'john@gmail.com',
       createdAt: new Date(),
       updatedAt: new Date(),
       onboarded: true,
     },
     {
-      name: "Brian",
-      email: "brian@gmail.com",
+      name: 'Brian',
+      email: 'brian@gmail.com',
       createdAt: new Date(),
       updatedAt: new Date(),
       onboarded: false,
     },
     {
-      name: "Noah",
-      email: "noah@gmail.com",
+      name: 'Noah',
+      email: 'noah@gmail.com',
       createdAt: new Date(),
       updatedAt: new Date(),
       onboarded: true,
     },
     {
-      name: "Reece",
-      email: "reece@gmail.com",
+      name: 'Reece',
+      email: 'reece@gmail.com',
       createdAt: new Date(),
       updatedAt: new Date(),
       onboarded: true,
-    }
-  ]
+    },
+  ];
 
   for (let user of users) {
     const res = await prisma.user.upsert({
       create: user,
       update: user,
       where: {
-        email: user.email
+        email: user.email,
       },
     });
   }
+};
 
-}
+const insertAnswers = async () => {
+  const answers = [
+    {
+      id: 1,
+      questionId: 1,
+      answer: `My favorite food is pizza. I love the combination of flavors from the melted cheese, tangy tomato sauce, and various toppings. It's a classic comfort food that always satisfies my cravings.`,
+      user: 'user1',
+      userId: 1,
+      createdAt: new Date(),
+    },
+    {
+      id: 2,
+      questionId: 1,
+      answer: `I'm a big fan of sushi. The freshness of the fish, the delicate flavors, and the beautiful presentation make it a culinary delight for me. Whether it's sashimi, nigiri, or maki rolls, I always enjoy the experience.`,
+      user: 'user2',
+      userId: 2,
+      createdAt: new Date(),
+    },
+    {
+      id: 3,
+      questionId: 1,
+      answer: `I have a sweet tooth, so my favorite food is chocolate. Whether it's a rich dark chocolate bar, a decadent chocolate cake, or even a simple chocolate chip cookie, I can't resist the indulgent taste and smooth texture.`,
+      user: 'user3',
+      userId: 3,
+      createdAt: new Date(),
+    },
+    {
+      id: 4,
+      questionId: 1,
+      answer: `I absolutely adore Mexican cuisine, so my favorite food is tacos. The combination of seasoned meat, fresh vegetables, salsa, and the crunch of the tortilla shell creates a burst of flavors and textures that I find incredibly satisfying.`,
+      user: 'user4',
+      userId: 4,
+      createdAt: new Date(),
+    },
+  ];
 
-insertUsers()
-  .then(insertQuestions);
+  for (let answer of answers) {
+    await prisma.answer.upsert({
+      where: { id: answer.id },
+      create: answer,
+      update: answer,
+    });
+  }
+};
 
+insertUsers().then(insertQuestions).then(insertAnswers);
